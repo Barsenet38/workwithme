54dae0a85726beb1c6744dc8fbd676d9fb0f8635
Barsenet39 <baydd24@gmail.com>
2025-09-01 14:51:05 +0300

    updated

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 // server.js
 const express = require("express");
 const dotenv = require("dotenv");
-const authRoutes = require("./src/routes/AuthRoutes");
+const cors = require("cors");
+const authRoutes = require("./src/routes/AuthRoutes.js"); // 👈 make sure file name matches
 
 dotenv.config();
 const app = express();
 
+// ✅ Enable CORS
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+// Middleware
 app.use(express.json());
 
 // Routes
